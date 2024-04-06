9291f5fd2415696936092d50a9bd3130ec34c84d
IlyaS0k <ilyasokolov63@mail.ru>
2024-04-06 14:17:57 +0300

    remove useless annotation

--- src/main/kotlin/ru/ilyasok/StickKs/repository/abstraction/IWordStatRepository.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/repository/abstraction/IWordStatRepository.kt
@@ -1,7 +1,3 @@
 package ru.ilyasok.StickKs.repository.abstraction
 
-import org.springframework.stereotype.Repository
-
-@Repository
-interface IWordStatRepository {
-}
+interface IWordStatRepository
--- src/main/kotlin/ru/ilyasok/StickKs/service/abstraction/IWordStatService.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/service/abstraction/IWordStatService.kt
@@ -1,6 +1,3 @@
 package ru.ilyasok.StickKs.service.abstraction;
 
-import org.springframework.stereotype.Service
-
-interface IWordStatService {
-}
+interface IWordStatService
