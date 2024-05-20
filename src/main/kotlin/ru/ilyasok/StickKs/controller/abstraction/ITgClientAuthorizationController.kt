5bf6650fb3d159370829370d178bddf856ec040e
IlyaS0k <ilyasokolov63@mail.ru>
2024-05-20 23:13:46 +0300

    interface for authorization controller

--- src/main/kotlin/ru/ilyasok/StickKs/controller/abstraction/ITgClientAuthorizationController.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/controller/abstraction/ITgClientAuthorizationController.kt
@@ -1,4 +1,10 @@
 package ru.ilyasok.StickKs.controller.abstraction
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+
 interface ITgClientAuthorizationController {
+    suspend fun authByPhone(): String
+    suspend fun submitPhone(@RequestBody phoneNumber: String?): ResponseEntity<String>
+    suspend fun submitLoginCode(@RequestBody loginCode: String?): ResponseEntity<String>
 }
