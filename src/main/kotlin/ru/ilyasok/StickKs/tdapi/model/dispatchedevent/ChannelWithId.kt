416cc3ca5c377a21935e667a4f97ec24e98f31e3
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-02-11 16:30:57 +0300

    remove

--- src/main/kotlin/ru/ilyasok/StickKs/tdapi/model/dispatchedevent/ChannelWithId.kt
+++
@@ -1,8 +0,0 @@
-package ru.ilyasok.StickKs.tdapi.model.dispatchedevent
-
-import kotlinx.coroutines.channels.Channel
-
-data class ChannelWithId<T> (
-    val id: Long,
-    val channel: Channel<T>
-)
