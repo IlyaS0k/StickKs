7ba0cebfdcc76376923f9f119f1f3ab89d056d87
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-05-01 15:28:07 +0300

    feature model

--- src/main/kotlin/ru/ilyasok/StickKs/model/Feature.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/model/Feature.kt
@@ -1,17 +1,15 @@
 package ru.ilyasok.StickKs.model
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.UUID
 
-@Document("users")
-class User(
+@Document("features")
+data class Feature(
     @Id
-    var id: UUID,
+    val id: UUID,
 
-    @Indexed(unique = true)
-    var phone: String,
+    val name: String,
 
-    
+    val code: String
 )
