6ad513c61971c981e6e04c7dfcece238ce6561e9
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-05-03 11:03:19 +0300

    deleted

--- src/main/kotlin/ru/ilyasok/StickKs/dsl/ConditionBlock.kt
+++
@@ -1,7 +0,0 @@
-package ru.ilyasok.StickKs.dsl
-
-import ru.ilyasok.StickKs.core.context.EventContext
-
-class ConditionBlock(
-    val event: EventContext,
-)
--- src/main/kotlin/ru/ilyasok/StickKs/tdapi/feature/context/TgNewTextMessageContext.kt
+++
@@ -1,9 +0,0 @@
-package ru.ilyasok.StickKs.tdapi.feature.context
-
-import ru.ilyasok.StickKs.core.context.EventContext
-import ru.ilyasok.StickKs.tdapi.TdApi
-
-class TgNewTextMessageContext(
-    val message: String = "",
-    val user: TdApi.User? = null,
-) : EventContext()
