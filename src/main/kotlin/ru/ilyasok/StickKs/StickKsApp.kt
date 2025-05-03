6eec64dcebe947dc808b790799056b0dc4020f1d
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-05-03 11:04:39 +0300

    dependency scanner

--- src/main/kotlin/ru/ilyasok/StickKs/StickKsApp.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/StickKsApp.kt
@@ -3,11 +3,13 @@ package ru.ilyasok.StickKs
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import ru.ilyasok.StickKs.dsl.DSLDependenciesProvider
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 class StickKsApp
 
 fun main(args: Array<String>) {
+    DSLDependenciesProvider.findDependencies()
     runApplication<StickKsApp>(*args)
 }
---
+++ src/main/kotlin/ru/ilyasok/StickKs/dsl/DSLDependenciesProvider.kt
@@ -0,0 +1,37 @@
+package ru.ilyasok.StickKs.dsl
+
+import io.github.classgraph.ClassGraph
+
+class DSLDependenciesProvider {
+
+    companion object {
+
+        private val DSL_FEATURE_ANNOTATION = FeatureDSL::class.java.name
+
+        private val dependencies = mutableListOf<String>(
+            "kotlin.time.Duration.Companion.*"
+        )
+
+        fun findDependencies() {
+            val scanResult = ClassGraph()
+                .enableAllInfo()
+                .scan()
+            val classes = scanResult.allClasses
+            val dslExtDependencies = classes.filter { c ->
+                c.hasDeclaredMethodAnnotation(DSL_FEATURE_ANNOTATION)
+            }.flatMap { c ->
+                c.methodInfo.filter { m ->
+                    m.annotationInfo.firstOrNull { a -> a.name == DSL_FEATURE_ANNOTATION } != null
+                }.map { m ->
+                    c.name.substringBeforeLast(".").plus(".${m.name}")
+                }
+            }
+
+            dependencies.addAll(dslExtDependencies)
+        }
+
+        fun provide(): List<String> = dependencies
+
+        fun provideAsString() = dependencies.joinToString(separator = "\n") { d -> "import $d" }
+    }
+}
