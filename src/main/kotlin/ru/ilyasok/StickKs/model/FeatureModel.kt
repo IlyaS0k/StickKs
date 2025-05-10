38fb56b6042bd238d0f198bd05922642d00b33f6
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-05-11 01:20:21 +0300

    feature model

---
+++ src/main/kotlin/ru/ilyasok/StickKs/model/FeatureModel.kt
@@ -0,0 +1,59 @@
+package ru.ilyasok.StickKs.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Version
+import org.springframework.data.relational.core.mapping.Table
+import ru.ilyasok.StickKs.dsl.Feature
+import ru.ilyasok.StickKs.dsl.FeatureBlock
+import ru.ilyasok.StickKs.dsl.FeatureMeta
+import java.time.Instant
+import java.util.UUID
+
+@Table("features")
+data class FeatureModel(
+    @Id
+    val id: UUID,
+
+    val name: String,
+
+    val code: String,
+
+    val createdAt: Instant = Instant.now(),
+
+    val lastModifiedAt: Instant = Instant.now(),
+
+    val lastSuccessExecutionAt: Instant? = null,
+
+    val lastFailedExecutionAt: Instant? = null,
+
+    val successExecutionsAmount: Long = 0L,
+
+    val failedExecutionsAmount: Long = 0L,
+
+    @Version
+    val version: Long? = null,
+
+) {
+    @org.springframework.data.annotation.Transient
+    var stability: FeatureStability? = FeatureStability.STABLE
+}
+
+enum class FeatureStability {
+    STABLE,
+    UNSTABLE,
+    BROKEN
+}
+
+fun FeatureModel.toFeature(featureBlock: FeatureBlock) = Feature(
+    id = this.id,
+    version = this.version!!,
+    feature = featureBlock,
+    meta = FeatureMeta(
+        createdAt = this.createdAt,
+        lastModifiedAt = this.lastModifiedAt,
+        lastSuccessExecutionAt = this.lastSuccessExecutionAt,
+        lastFailedExecutionAt = this.lastFailedExecutionAt,
+        successExecutionsAmount = this.successExecutionsAmount,
+        failedExecutionsAmount = this.failedExecutionsAmount
+    ),
+)
