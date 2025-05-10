d9c5a7145ce6296f7d85a441562435c07f27204f
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-05-11 01:16:06 +0300

    removed annotation

--- src/main/kotlin/ru/ilyasok/StickKs/StickKsApp.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/StickKsApp.kt
@@ -2,12 +2,10 @@ package ru.ilyasok.StickKs
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 import ru.ilyasok.StickKs.dsl.DSLDependenciesProvider
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
-open class StickKsApp
+class StickKsApp
 
 fun main(args: Array<String>) {
     DSLDependenciesProvider.findDependencies()
