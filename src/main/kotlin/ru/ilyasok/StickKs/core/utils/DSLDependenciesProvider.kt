c7eb20bf83a71cdf28a10bd0ba62829676088cec
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-05-22 18:14:19 +0300

    file with external dependencies for dsl

--- src/main/kotlin/ru/ilyasok/StickKs/core/utils/DSLDependenciesProvider.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/core/utils/DSLDependenciesProvider.kt
@@ -2,21 +2,24 @@ package ru.ilyasok.StickKs.core.utils
 
 import io.github.classgraph.ClassGraph
 import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import ru.ilyasok.StickKs.dsl.FeatureDSL
+import java.io.File
+import java.util.UUID
 
 @Component
-class DSLDependenciesProvider {
-
+class DSLDependenciesProvider(
+    @param:Value("\${dsl-dependencies}")
+    private val dslDependenciesPath: String,
+) {
     private val dslFeatureAnnotation = FeatureDSL::class.java.name
 
-    private val dependencies = mutableListOf(
-        "kotlin.time.Duration.Companion.milliseconds",
-        "kotlin.time.Duration.Companion.seconds",
-        "kotlin.time.Duration.Companion.minutes",
-        "kotlin.time.Duration.Companion.hours",
-        "kotlin.time.Duration.Companion.days",
-    )
+    private val dependencies: MutableList<String> = if (File(dslDependenciesPath).exists()) {
+        File(dslDependenciesPath).readLines().toMutableList()
+    } else {
+        mutableListOf()
+    }
 
     @PostConstruct
     fun findDependencies() {
