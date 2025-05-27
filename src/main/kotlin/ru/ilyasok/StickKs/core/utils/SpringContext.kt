3fb1761ed6af8308b749d3ae712ec9c4867a9371
sokolov_ii@crc.ru <ilyasokolov63@mail.ru>
2025-05-27 19:21:11 +0300

    clock

--- src/main/kotlin/ru/ilyasok/StickKs/core/utils/SpringContext.kt
+++ src/main/kotlin/ru/ilyasok/StickKs/core/utils/SpringContext.kt
@@ -3,6 +3,7 @@ package ru.ilyasok.StickKs.core.utils
 import jakarta.annotation.PostConstruct
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
+import java.time.Clock
 
 @Component
 class SpringContext(private val context: ApplicationContext) {
@@ -13,10 +14,15 @@ class SpringContext(private val context: ApplicationContext) {
         fun <T> getBean(beanClass: Class<T>): T {
             return SPRING_CONTEXT.context.getBean(beanClass)
         }
+
+        fun clock(): Clock {
+            return getBean(Clock::class.java)
+        }
     }
 
     @PostConstruct
     fun initSpringContext() {
         SPRING_CONTEXT = this
     }
+
 }
